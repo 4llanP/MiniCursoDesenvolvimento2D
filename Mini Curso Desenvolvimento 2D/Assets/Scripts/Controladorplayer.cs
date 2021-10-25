b2f6382c08f4cb86a4fbb649d7075db93a207829
All4nP <66078686+4llanP@users.noreply.github.com>
2021-10-25 05:14:30 -0300

    0.0.01
    aula 00 até aula 06

---
+++ Mini Curso Desenvolvimento 2D/Assets/Scripts/Controladorplayer.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controladorplayer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed, jumpPower, radiusCircle, airTime;
+    [SerializeField]
+    private Transform feetPosition;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private GameObject prefHitbox;
+
+    private Rigidbody2D r2;
+    private Animator anim;
+    private float moveInput, timer;
+    private bool pressJump, possJump, startTime, isGrounded, inAtaque = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        r2 = GetComponent<Rigidbody2D>();
+        timer = airTime;
+    }
+
+    private void Update(){
+          
+        //Animator
+        anim.SetInteger("MovimentoX", (int)moveInput);
+        anim.SetFloat("MovimentoY", r2.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+
+        //Ataque
+        if (Input.GetKeyDown(KeyCode.J) && !inAtaque && isGrounded){
+            anim.SetTrigger("Ataque");
+            inAtaque = true;
+        }
+
+        //Primeira parte do pulo
+        if (Input.GetButtonDown("Jump") && !inAtaque)
+        {
+            pressJump = true;
+        }
+        if (pressJump && Input.GetButtonUp("Jump"))
+        {
+            possJump = true;
+        }
+        if (startTime)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                possJump = true;
+            }
+        }
+
+    }
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, radiusCircle, groundLayer);
+
+        // movimentacao
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (inAtaque){
+            moveInput = 0;
+            r2.velocity = new Vector2(0, 0);
+        }
+
+        r2.velocity = new Vector2(moveInput * speed, r2.velocity.y);
+
+        //Girar
+        if (moveInput > 0){
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }else if (moveInput < 0){
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+
+        //Segunda parte do pulo
+        if (pressJump && isGrounded)
+        {
+            startJump();
+        }
+        else if (possJump)
+        {
+            stopJump();
+        }
+
+        void startJump(){
+            r2.gravityScale = 3;
+            r2.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+            pressJump = false;
+        }
+        void stopJump()
+        {
+            r2.gravityScale = 5;
+            possJump = false;
+            timer = airTime;
+        }
+    }
+
+    public void fimAtaque(){
+        inAtaque = false;
+    }
+}
